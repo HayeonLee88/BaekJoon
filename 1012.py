cf4fa81213733439bf26fc9b0dc9ea870b5fab4f
HayeonLee88 <hayeon0808@swu.ac.kr>
2023-05-31 01:22:44 +0900

    # 1012번: 유기농 배추

---
+++ 1012.py
@@ -0,0 +1,35 @@
+# 1012번: 유기농 배추
+# DFS
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(30001)
+
+def DFS(x, y):
+    if x < 0 or x >= m or y < 0 or y >= n:
+        return False
+    if graph[x][y]:
+        graph[x][y] = 0 # 0으로 바꿔야 무한 루프에 빠지지 않는다.
+        DFS(x - 1, y)
+        DFS(x + 1, y)
+        DFS(x, y - 1)
+        DFS(x, y + 1)
+        return True
+    return False
+
+T = int(input())
+for test_case in range(T):
+    m, n, k = map(int, input().split())
+    graph = [[0] * n for _ in range(m)]
+
+    for i in range(k):
+        x, y = map(int, input().split())
+        graph[x][y] = 1
+
+    answer = 0
+    for i in range(m):
+        for j in range(n):
+            if DFS(i, j):
+                answer += 1
+
+    print(answer)

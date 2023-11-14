f058838331dee7647c11f3ae7bbed5e0bd3d023d
이하연_T6140 <83398511+HayeonLee88@users.noreply.github.com>
2023-11-14 23:14:32 +0900

    [Bronze II] Title: 방 배정, Time: 44 ms, Memory: 31120 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.py
@@ -0,0 +1,16 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+total_students, max_num = map(int, input().split())
+students = [[0, 0] for _ in range(7)]
+
+for i in range(total_students):
+    gender, grade = map(int, input().split())
+    students[grade][gender] += 1
+
+answer = 0
+for girl, boy in students:
+    answer += girl // max_num + girl % max_num + boy // max_num + boy % max_num
+
+print(answer)

a2387c37d8591c3bcf5d3f23775f459a9e1746fa
이하연_T6140 <83398511+HayeonLee88@users.noreply.github.com>
2023-11-16 21:10:44 +0900

    [Silver II] Title: 키로거, Time: 996 ms, Memory: 43616 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.py
+++ 백준/Silver/5397. 키로거/키로거.py
@@ -9,13 +9,16 @@ for test_case in range(T):
     # 커서의 왼쪽에 있는 문자를 담는 덱, 커서의 오른쪽에 있는 문자를 담는 덱
     left, right = deque(), deque()
     for c in cmd:
-        if c == '<' and left: # 커서의 위치가 맨 앞이 아니라면 커서 왼쪽으로 움직이기
-            right.appendleft(left.pop())
-        elif c == '>' and right: # 커서의 위치가 마지막이 아니라면 커서 오른쪽으로 움직이기
-            left.append(right.popleft())
-        elif c == '-' and left: # 커서의 위치가 맨 앞이 아니라면 커서 왼쪽 문자 지우기
-            left.pop()
-        elif c.isdigit() or c.isalpha(): # 알파벳이나 숫자가 입력되면 추가하기
+        if c == '<': 
+            if left: # 커서의 위치가 맨 앞이 아니라면 커서 왼쪽으로 움직이기
+                right.appendleft(left.pop())
+        elif c == '>':
+            if right: # 커서의 위치가 마지막이 아니라면 커서 오른쪽으로 움직이기
+                left.append(right.popleft())
+        elif c == '-':
+            if left: # 커서의 위치가 맨 앞이 아니라면 커서 왼쪽 문자 지우기
+                left.pop()
+        else: # 알파벳이나 숫자가 입력되면 추가하기
             left.append(c)
     print(''.join(left) + ''.join(right))
     

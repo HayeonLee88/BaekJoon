5e77563405c6913d8fffb8619005e70b42b52845
이하연_T6140 <83398511+HayeonLee88@users.noreply.github.com>
2023-11-17 16:51:37 +0900

    [Silver II] Title: 에디터, Time: 288 ms, Memory: 159712 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -8,13 +8,16 @@ n = int(input())
 
 for _ in range(n):
     cmd = input()
-    if cmd[0] == 'L' and q_left:
-        q_right.appendleft(q_left.pop())
-    elif cmd[0] == 'D' and q_right:
-        q_left.append(q_right.popleft())
-    elif cmd[0] == 'B' and q_left:
-        q_left.pop()
-    elif cmd[0] == 'P':
+    if cmd[0] == 'L':
+        if q_left:
+            q_right.appendleft(q_left.pop())
+    elif cmd[0] == 'D':
+        if q_right:
+            q_left.append(q_right.popleft())
+    elif cmd[0] == 'B': 
+        if q_left:
+            q_left.pop()
+    else:
         q_left.append(cmd[-1])
 
 print(''.join(c for c in q_left), end='')

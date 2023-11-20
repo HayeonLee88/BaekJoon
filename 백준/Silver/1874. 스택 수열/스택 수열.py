16eec75ad0eb32ebfc86a86b9392c3c186e97687
이하연_T6140 <83398511+HayeonLee88@users.noreply.github.com>
2023-11-20 23:47:16 +0900

    [Silver II] Title: 스택 수열, Time: 200 ms, Memory: 38324 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,30 @@
+import sys
+from collections import deque
+
+input = lambda: sys.stdin.readline().rstrip()
+
+n = int(input())
+stack = deque() # 수열을 만들 때 사용하는 스택
+answer = deque() # 수열을 만들 때의 연산을 저장하는 변수
+end = 0 # stack에 append한 제일 큰 수
+
+for i in range(n):
+    num = int(input())
+    if end < num:
+        for i in range(end + 1, num + 1):
+            stack.append(i)
+            answer.append('+')
+        stack.pop()
+        answer.append('-')
+        end = num
+    elif end > num:
+        if stack.pop() != num:
+            answer = ["NO"]
+            break
+        else:
+            answer.append('-')
+    else:
+        stack.pop()
+        answer.append('-')
+
+print(*answer, sep="\n")

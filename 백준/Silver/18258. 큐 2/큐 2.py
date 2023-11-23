c20a7ab06df610bd116960fc769c6c85b311cbde
이하연_T6140 <83398511+HayeonLee88@users.noreply.github.com>
2023-11-23 21:27:17 +0900

    [Silver IV] Title: 큐 2, Time: 1112 ms, Memory: 254552 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -8,15 +8,25 @@ q = deque()
 
 for _ in range(n):
     cmd = list(input().split())
-    if cmd[0] == 'pop':
-        print(q.popleft() if q else -1)
-    elif cmd[0] == 'size':
+    if cmd[0] == 'size': # q에 정수가 있든 없든 상관없이 실행되는 명령
         print(len(q))
-    elif cmd[0] == 'empty':
-        print(0 if q else 1)
-    elif cmd[0] == 'front':
-        print(q[0] if q else -1)
-    elif cmd[0] == 'back':
-        print(q[-1] if q else -1)
-    else: # push X
-        q.append(cmd[-1])
+    else:
+        if q:
+            if cmd[0] == 'pop':
+                print(q.popleft())
+            elif cmd[0] == 'front':
+                print(q[0])
+            elif cmd[0] == 'back':
+                print(q[-1])
+            elif cmd[0] == 'empty':
+                print(0)
+            else:  # push X
+                q.append(cmd[-1])
+        else:
+            if cmd[0] in ['pop', 'front', 'back']:
+                print(-1)
+            elif cmd[0] == 'empty':
+                print(1)
+            else: # push X
+                q.append(cmd[-1])
+                

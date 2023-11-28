8edf3043302e2956f2c375b201a9db21e5345a2a
이하연_T6140 <83398511+HayeonLee88@users.noreply.github.com>
2023-11-28 15:09:58 +0900

    [Gold V] Title: AC, Time: 3616 ms, Memory: 38368 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -8,30 +8,29 @@ for _ in range(T):
     n = int(input())
     array = input()
 
-    if n:
-        array = list(array[1:len(array) - 1].split(','))
-    else:
-        array = []
-
-    error_check = False
-    reversed_check = False
-    for c in cmd:
-        if c == "R":
-            reversed_check = not reversed_check
-        else:
-            if array: # 비어있는지 체크
-                if reversed_check: # 뒤집혔다면
-                    array.pop()
-                else:
-                    array.pop(0)
-            else:
-                error_check = True
-                break
-
-    if error_check:
+    if n < cmd.count('D'): # 배열의 원소 개수보다 버리는 개수가 더 많은 때
         print("error")
-    elif reversed_check:
-        print('['+','.join(array[::-1]) + ']')
+
     else:
-        print('['+','.join(array) + ']')
-        
+        if n: # 원소의 개수가 0이 아니라면
+            array = list(array[1:len(array) - 1].split(','))
+
+            reversed_check = False
+            for c in cmd:
+                if c == "R":
+                    reversed_check = not reversed_check
+                else:
+                    if array:  # 비어있는지 체크
+                        if reversed_check:  # 뒤집혔다면
+                            array.pop()
+                        else:
+                            array.pop(0)
+
+            if reversed_check:
+                print('[' + ','.join(array[::-1]) + ']')
+            else:
+                print('[' + ','.join(array) + ']')
+
+        else: # 원소의 개수가 0이라면
+            print('[]')
+            

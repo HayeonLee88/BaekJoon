5e4173be4942a589f0e9bbe11f798d274ba99c56
Hayeon Lee <83398511+HayeonLee88@users.noreply.github.com>
2024-04-23 00:45:46 +0900

    [Silver I] Title: 1로 만들기 2, Time: 616 ms, Memory: 78624 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
@@ -1,18 +1,17 @@
 n = int(input())
-cnt_n_prev = [[0, 0] for i in range(n + 1)] # 1이 만들어기지 위한 횟수와 이전의 숫자를 담는 리스트
-
+cnt = [0] * (n + 1) # 1이 만들어기지 위한 최소 횟수를 담는 리스트
+prev = [0] * (n + 1) # 이전의 숫자를 담는 리스트
 for i in range(2, n + 1):
-    cnt_n_prev[i][0] = cnt_n_prev[i - 1][0] + 1 # 이전의 수에서 1을 더하는 경우
-    cnt_n_prev[i][1] = i - 1
-    if i % 2 == 0 and cnt_n_prev[i][0] > cnt_n_prev[i // 2][0] + 1: # 2의 배수인 경우
-        cnt_n_prev[i][0] = cnt_n_prev[i // 2][0] + 1
-        cnt_n_prev[i][1] = i // 2
-    if i % 3 == 0 and cnt_n_prev[i][0] > cnt_n_prev[i // 3][0] + 1: # 3의 배수인 경우
-        cnt_n_prev[i][0] = cnt_n_prev[i // 3][0] + 1
-        cnt_n_prev[i][1] = i // 3
-        
-print(cnt_n_prev[n][0])
-print(n, end=' ')
-while cnt_n_prev[n][1] != 0:
-    print(cnt_n_prev[n][1], end=' ')
-    n = cnt_n_prev[n][1]
+    cnt[i] = cnt[i - 1] + 1 # 이전의 수에서 1을 더하는 경우
+    prev[i] = i - 1
+    if i % 2 == 0 and cnt[i] > cnt[i // 2] + 1: # 2의 배수인 경우
+        cnt[i] = cnt[i // 2] + 1
+        prev[i] = i // 2
+    if i % 3 == 0 and cnt[i] > cnt[i // 3] + 1: # 3의 배수인 경우
+        cnt[i] = cnt[i // 3] + 1
+        prev[i] = i // 3
+
+print(cnt[n])
+while n > 0:
+    print(n, end=" ")
+    n = prev[n]

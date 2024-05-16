ec7a0c518cc27a73f12993a290c6a7b3a4344fbd
Hayeon Lee <83398511+HayeonLee88@users.noreply.github.com>
2024-05-17 00:04:28 +0900

    [Silver III] Title: N과 M (1), Time: 172 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,19 +1,19 @@
 n, m = map(int, input().split())
-nums = list(range(1, n + 1))
+
+nums = [i for i in range(1, n + 1)]
 visited = [False] * (n + 1)
 answer = [0] * m
 
-def backtrack(n, m, len):
-  global visited, answer
-  if len == m:
-    print(*answer)
-    return
-  else:
-    for i in range(1, n + 1):
-      if len < m and not visited[i]:
-        answer[len] = i
-        visited[i] = True
-        backtrack(n, m, len + 1)
-        visited[i] = False
+def backtracking(i):
+    if i == m:
+        print(*answer)
+        return
+    for num in nums:
+        if visited[num]: # 이미 방문한 숫자라면 continue
+            continue
+        answer[i] = num
+        visited[num] = True
+        backtracking(i + 1) # i번째 이후의 숫자를 담기 위해 재귀 호출 
+        visited[num] = False # i번째 이후에 num이 들어가기 위해 False로 바꾸기
 
-backtrack(n, m, 0)
+backtracking(0)

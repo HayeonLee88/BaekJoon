c9b562d5b824eca4e8b02cbd8e2df16f7ed6b74e
Hayeon Lee <83398511+HayeonLee88@users.noreply.github.com>
2024-05-18 18:02:57 +0900

    [Silver II] Title: 색종이 만들기, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -3,25 +3,23 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 
 n = int(input())
-graph = [list(map(int, input().split())) for i in range(n)]
+graph = [list(map(int, input().split())) for _ in range(n)]
+color_dict = {0:0, 1:0}
 
-answer = {0: 0, 1: 0}
+def search(n, x, y):
+	global graph
+	color = graph[x][y]
 
-def search(x, y, n):
-    global graph
-    global answer
-    now = graph[x][y]
-    for i in range(x, x + n):
-        for j in range(y, y + n):
-            if graph[i][j] != now:
-                n //= 2
-                search(x, y, n)
-                search(x + n, y, n)
-                search(x, y + n, n)
-                search(x + n, y + n, n)
-                return False
-    answer[now] += 1
-    return True
+	for i in range(x, x + n):
+		for j in range(y, y + n):
+			if graph[i][j] != color:
+				n //= 2
+				search(n, x, y)
+				search(n, x, y + n)
+				search(n, x + n, y)
+				search(n, x + n, y + n)
+				return 
+	color_dict[color] += 1
 
-search(0, 0, n)
-print(f'{answer[0]}\n{answer[1]}', end='')
+search(n, 0, 0)
+print(*color_dict.values(), sep='\n')

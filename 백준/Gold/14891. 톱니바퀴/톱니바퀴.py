788f90e33eed4bac22a9087f37fb5eb21361a317
Hayeon Lee <83398511+HayeonLee88@users.noreply.github.com>
2024-07-16 13:11:59 +0900

    [Gold V] Title: 톱니바퀴, Time: 64 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
@@ -9,22 +9,22 @@ for i in range(1, 5):
 state = []
 def rotate(start, end, dir1, dir2): 
     '''
-    start, end: 번호 내를 탐색
+    num: 기준이 되는 톱니바퀴 번호
     dir1: -1 이면 왼쪽 1이면 오른쪽 방향 톱니 탐색
     dir2: 톱니가 돌아갈 방향
     '''
     global state
-    d = dir2 # 시계, 반시계 방향
+    d = dir2
     for i in range(start, end, dir1):
         d *= -1
         if dir1 == 1:
-            if state[i - 1] == 1:
+            if state[i + dir1 * -1] == 1:
                 wheels[i].rotate(d)
             else:
                 break
         else:
             if state[i] == 1:
-                wheels[i].rotate(d)
+               wheels[i].rotate(d)
             else:
                 break
         
@@ -34,8 +34,8 @@ for i in range(n):
     state = [-1, wheels[1][2] + wheels[2][-2], wheels[2][2] + wheels[3][-2], wheels[3][2] + wheels[4][-2]]
     num, dir = map(int, input().split())
     wheels[num].rotate(dir)
-    rotate(num -1, 0, -1, dir) # 왼쪽 방향 탐색
-    rotate(num + 1, 5, 1, dir) # 오른쪽 방향 탐색
+    rotate(num -1, 0, -1, dir)
+    rotate(num + 1, 5, 1, dir)
 
 for j in range(4):
      if wheels[j + 1][0] == 1:

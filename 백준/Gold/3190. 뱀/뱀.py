bbdb39559a4900ef5cb4d024fcdb8f44ba86a8ca
Hayeon Lee <83398511+HayeonLee88@users.noreply.github.com>
2024-08-06 08:27:39 +0900

    [Gold IV] Title: 뱀, Time: 52 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -1,54 +1,58 @@
+import sys
 from collections import deque
 
-n = int(input()) # 보드의 크기
-graph = [[0] * (n + 1) for _ in range(n + 1)]
+input = lambda:sys.stdin.readline().rstrip()
 
-k = int(input()) # 사과의 개수
+n = int(input())
+# 보드
+graph = [[-1] * (n + 2)]
+
+for i in range(1, n + 1):
+    graph.append([-1])
+    graph[i].extend([0] * n)
+    graph[i].append(-1)
+
+graph.append([-1] * (n + 2))
+
+# 사과 위치 
+k = int(input())
 for i in range(k):
     x, y = map(int, input().split())
-    graph[x][y] = 1 # 사과는 1
+    graph[x][y] = 1
 
-l = int(input()) # 방향 전환 횟수: x초 뒤, L은 왼쪽, D는 오른쪽으로 90도 회전
-info = [(input().split()) for _ in range(l)]
+l = int(input())
+# 방향 전환 정보
+dirs = deque(list(input().split()) for _ in range(l))
 
-# 이동 방향을 담는 변수
-# +1 왼쪽 회전, -1 오른쪽 회전
-dx = [0, -1, 0, 1]
+# 이동 방향 (오른쪽부터 시계방향)
+dx = [0, 1, 0, -1] 
 dy = [1, 0, -1, 0]
-i, index = 0, 0 # i: 회전 방향, index: 회전 정보 인덱스
 
-# 회전 함수: +1 왼쪽 회전, -1 오른쪽 회전
-def rotate(c):
-  global i
-  if c == 'L':
-    i = (i + 1) % 4
-  else:
-    i = (i - 1) % 4
+dir = {'L': -1, 'D': 1}
 
-# 뱀의 몸의 위치를 저장할 덱
-snake = deque()
-snake.append([1,1])
-time = 0
-x, y = 1, 1 # 뱀의 현재 머리 위치
-graph[x][y] = 2 # 뱀이 있는 곳은 2
-while True:
-    # 이동한 머리의 위치
-    nx, ny = x + dx[i], y + dy[i]
-    # 벽에 부딪히거나 자신의 몸에 닿았을 때
-    if nx <= 0 or nx > n or ny <= 0 or ny > n or graph[nx][ny] == 2:
-        time += 1 # 1초를 더한 후 종료
-        break
-    # 뱀의 머리 위치에 사과가 없다면 꼬리 꺼내기
-    if graph[nx][ny] == 0:
-        px, py = snake.popleft()
-        graph[px][py] = 0
-    # 뱀의 머리가 있는 칸을 2로 수정, 뱀 머리 칸 덱에 추가
-    graph[nx][ny] = 2  
-    snake.append([nx, ny])
-    x, y = nx, ny # 뱀의 머리 위치 수정
-    time += 1
-    # 방향 정보의 시간과 현재 시간이 일치할 때
-    if index < l and time == int(info[index][0]):
-        rotate(info[index][1])
-        index += 1
-print(time)   
+def dos(x, y):
+    q = deque()
+    q.append((1, 1))
+    cnt = 0 # 시간
+    idx = 0 # 이동방향 인덱스
+    x, y = q[0]
+    while True:
+        cnt += 1
+        nx = x + dx[idx]
+        ny = y + dy[idx]
+        if (nx, ny) in q or graph[nx][ny] == -1:
+            break
+        if graph[nx][ny] == 1:
+            graph[nx][ny] = 0
+        else:
+            q.popleft()
+        q.append((nx, ny))
+        x, y = nx, ny
+        if dirs:
+            if cnt == int(dirs[0][0]):
+                _, c = dirs.popleft()
+                idx += dir[c]
+                idx %= 4
+    return cnt
+
+print(dos(1, 1))

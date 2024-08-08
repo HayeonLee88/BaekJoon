40cc601237bd51fbdbcc0f9a391acf7ebd2caa0d
Hayeon Lee <83398511+HayeonLee88@users.noreply.github.com>
2024-08-08 17:29:12 +0900

    [Silver II] Title: 유기농 배추, Time: 44 ms, Memory: 31420 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,39 +1,34 @@
 import sys
 
-input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(100000)
+input = lambda:sys.stdin.readline().rstrip()
 
-sys.setrecursionlimit(100001)
-
-t = int(input())
-
-
-def dfs(x, y):
+T = int(input())
+n, m, k = 0, 0, 0
+graph = []
+def DFS(x, y):
     if x < 0 or x >= n or y < 0 or y >= m:
         return False
-    if graph[x][y]: # 배추가 심어져 있다면
-        graph[x][y] = 0
-        dfs(x - 1, y)
-        dfs(x + 1, y)
-        dfs(x, y - 1)
-        dfs(x, y + 1)
-        return True
+    else:
+        if graph[x][y] == 1:
+            graph[x][y] = 0
+            DFS(x + 1, y)
+            DFS(x, y + 1)
+            DFS(x - 1, y)
+            DFS(x, y - 1)
+            return True
     return False
 
-answer = []
-for _ in range(t):
-    tmp = 0
-    m, n, k = map(int, input().split())
+
+for test_case in range(T):
+    n, m, k = map(int, input().split())
     graph = [[0] * m for _ in range(n)]
-
-
-    for _ in range(k):
-        y, x = map(int, input().split())
+    cnt = 0
+    for i in range(k):
+        x, y = map(int, input().split())
         graph[x][y] = 1
-
     for i in range(n):
         for j in range(m):
-            if dfs(i, j):
-                tmp += 1
-    answer.append(tmp)
-
-print(*answer, sep='\n')
+            if DFS(i, j):
+                cnt += 1
+    print(cnt)

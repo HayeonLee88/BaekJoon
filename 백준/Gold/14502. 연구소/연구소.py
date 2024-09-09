a69129258475830b05b9c3c756e233970657d790
Hayeon Lee <83398511+HayeonLee88@users.noreply.github.com>
2024-09-09 20:39:04 +0900

    [Gold IV] Title: 연구소, Time: 1596 ms, Memory: 34280 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,54 +1,66 @@
 import sys
-import copy
 from collections import deque
 from itertools import combinations
 
-n, m = map(int, input().split())
-graph = []
+input = lambda:sys.stdin.readline().rstrip()
 
-virus = []
-empty = []
-empty_cnt = 0
+graph = [] # 연구소의 지도를 담는 리스트
+viruses = [] # 바이러스의 위치를 담는 리시트
+empty = [] # 빈 칸의 위치를 담는 리스트
+
+n, m = map(int, input().split())
+total = -3 # 빈 칸의 총 수
+
 for i in range(n):
-    row = list(map(int, sys.stdin.readline().rstrip().split()))
-    graph.append(row)
+    graph.append(list(map(int, input().split())))
     for j in range(m):
-        x = row[j]
+        x = graph[i][j]
         if x == 2:
-            virus.append((i, j))
+            viruses.append([i, j])
         elif x == 0:
-            empty.append((i, j))
-            empty_cnt += 1
+            total += 1
+            empty.append([i, j])
+
+# 새로 새울 3개의 벽의 조합
+walls = list(combinations(empty, 3))
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def bfs(x, y):
-    global cnt
+def bfs():
+    global total, viruses
+    cnt = 0
     q = deque()
-    q.append((x, y))
+    for virus in viruses:
+        q.append(virus)
     while q:
         x, y = q.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m:
-                if tmp_graph[nx][ny] == 0:
-                    tmp_graph[nx][ny] = 2
-                    q.append((nx, ny))
-                    cnt -= 1
-
-b_walls = list(combinations(empty, 3))
+            # map을 벗어난 경우
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            # 빈 칸이면서 바이러스가 퍼지지 않은 경우
+            if graph[nx][ny] == 0 and not visited[nx][ny]:
+                q.append([nx, ny])
+                visited[nx][ny] = True
+                cnt += 1
+    return total - cnt
 
 answer = 0
-for a, b, c in b_walls:
-    tmp_graph = copy.deepcopy(graph)
-    tmp_graph[a[0]][a[1]] = 1 # 벽 세우기
-    tmp_graph[b[0]][b[1]] = 1
-    tmp_graph[c[0]][c[1]] = 1
-    cnt = empty_cnt - 3 # 새로 새운 벽 3개 빼기
-    for x, y in virus:
-        bfs(x, y)
-    answer = max(answer, cnt)
+for a, b, c in walls:
+    visited = [[False] * m for _ in range(n)]
+    # 새로운 벽 세우기
+    graph[a[0]][a[1]] = 1
+    graph[b[0]][b[1]] = 1
+    graph[c[0]][c[1]] = 1
+    
+    # 최대 안전 영역 구하기
+    answer = max(answer, bfs())
+   
+    graph[a[0]][a[1]] = 0
+    graph[b[0]][b[1]] = 0
+    graph[c[0]][c[1]] = 0
 
 print(answer)

ada4678e8dc71258c1adde507bdd29dbc732f51d
Hayeon Lee <83398511+HayeonLee88@users.noreply.github.com>
2024-10-03 22:05:36 +0900

    [Silver III] Title: 퇴사, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,11 +1,20 @@
-n = int(input())
-data = [list(map(int, input().split())) for _ in range(n)]
-dp = [0] * (n + 1)
-for i in range(n - 1, -1, -1):
-    T, P = data[i]
-    if i + T > n: # 마지막 날까지 일을 마칠 수 없다면
-        dp[i] = dp[i + 1]
-    else:
-        dp[i] = max(P + dp[i + T], dp[i + 1])
+import sys
+input = lambda:sys.stdin.readline().rstrip()
 
-print(dp[0])
+n = int(input())
+counsel = [list(map(int, input().split())) for _ in range(n)]
+
+stack = [0] * (n + 1)
+
+for i in range(n - 1, -1, -1):
+    if n - i >= counsel[i][0]:
+        tmp = counsel[i][1] + stack[i + counsel[i][0]]
+        if tmp > stack[i + 1]:
+            stack[i] = tmp
+        else:
+            stack[i] = stack[i + 1]
+
+    else:
+        stack[i] = stack[i + 1]
+
+print(stack[0])

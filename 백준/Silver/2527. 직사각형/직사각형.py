cd9a4669e93dd7812531307abd6005e7a3eb7333
Hayeon Lee <83398511+HayeonLee88@users.noreply.github.com>
2024-11-15 10:21:50 +0900

    [Silver I] Title: 직사각형, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2527. 직사각형/직사각형.py
+++ 백준/Silver/2527. 직사각형/직사각형.py
@@ -6,16 +6,11 @@ def search(a, b):
     elif (a[2] == b[0] or a[0] == b[2]) and (a[1] < b[3] and a[3] > b[1]) or (a[3] == b[1] or a[1] == b[3]) and (a[0] < b[2] and a[2] > b[0]):
         return 'b'
     else:
-        return 'z'
+        return 'a'
 
 for _ in range(4):
     x, y, p, q, a, b, l, m = map(int, input().split())
     s1 = [x, y, p, q]
     s2 = [a, b, l, m]
 
-    tmp1 = search(s1, s2)    
-    tmp2 = search(s2, s1)
-    if tmp1 == 'z' and tmp2 == 'z':
-        print('a')
-    else:
-        print(tmp1 if tmp1 < tmp2 else tmp2)
+    print(search(s1, s2)) 

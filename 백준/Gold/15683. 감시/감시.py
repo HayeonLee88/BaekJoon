a6757ebe1c79078d60546b2511507b45f9065183
Hayeon Lee <83398511+HayeonLee88@users.noreply.github.com>
2024-11-26 10:04:46 +0900

    [Gold III] Title: 감시, Time: 1264 ms, Memory: 143892 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.py
+++ 백준/Gold/15683. 감시/감시.py
@@ -1,9 +1,11 @@
 import sys
 import copy
+
+
 input = lambda:sys.stdin.readline().rstrip()
 
 n, m = map(int, input().split())
-# 시계 방향으로 90도씩 회전
+
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 graph = []
@@ -16,7 +18,6 @@ for i in range(n):
         if x in [1, 2, 3, 4, 5]:
             cctvs.append((x, [i, j]))
 
-tmp = copy.deepcopy(graph)
 
 def cctv(x, y, i, graph):
     i = i % 4
@@ -29,8 +30,10 @@ def cctv(x, y, i, graph):
             break
         elif graph[nx][ny] == 0:
             graph[nx][ny] = -1
+
         x = nx
         y = ny
+
 
 def dfs(i, graph):
     global answer
@@ -40,11 +43,12 @@ def dfs(i, graph):
             cnt += row.count(0)
         answer = min(answer, cnt)
         return
-
+    
     num = cctvs[i][0]
-    x, y = cctvs[i][1][0], cctvs[i][1][1]
+    x, y = cctvs[i][1]
     for dir in range(4):
         tmp = copy.deepcopy(graph)
+        # CCTV 종류에 따른 감시 방법
         if num == 1:
             cctv(x, y, dir, tmp)
         elif num == 2:

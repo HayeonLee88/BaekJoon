28232beeaf1c6b94c381b613c7b334d69af3f47c
Hayeon Lee <83398511+HayeonLee88@users.noreply.github.com>
2024-12-17 09:54:45 +0900

    [Gold V] Title: AC, Time: 148 ms, Memory: 41640 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -1,36 +1,37 @@
 import sys
+from collections import deque
 
-input = lambda: sys.stdin.readline().rstrip()
+
+input = lambda:sys.stdin.readline().rstrip()
 
 T = int(input())
-for _ in range(T):
+
+for test_case in range(T):
     cmd = input()
     n = int(input())
-    array = input()
+    array = deque(input()[1:-1].split(','))
 
-    if n < cmd.count('D'): # 배열의 원소 개수보다 버리는 개수가 더 많은 때
-        print("error")
-
+    # 배열의 길이보다 연산 'D'가 많을 때
+    if n < cmd.count('D'):
+        print('error')
     else:
-        if n: # 원소의 개수가 0이 아니라면
-            array = list(array[1:len(array) - 1].split(','))
-
-            reversed_check = False
-            for c in cmd:
-                if c == "R":
-                    reversed_check = not reversed_check
-                else:
-                    if array:  # 비어있는지 체크
-                        if reversed_check:  # 뒤집혔다면
-                            array.pop()
-                        else:
-                            array.pop(0)
-
-            if reversed_check:
-                print('[' + ','.join(array[::-1]) + ']')
+        reversed = False
+        for x in cmd:
+            if x == 'R':
+                reversed = not reversed
             else:
-                print('[' + ','.join(array) + ']')
-
-        else: # 원소의 개수가 0이라면
-            print('[]')
-            
+                # 비어 있지 않을 때
+                if array:
+                    # 뒤집혔다면
+                    if reversed:
+                        array.pop()
+                    # 뒤집히지 않았다면
+                    else:
+                        array.popleft()
+                else:
+                    array = 'error'
+                    break
+        if array == 'error':
+            print(array)
+        else:
+            print('['+ ','.join(list(array)[::-1]) +']' if reversed else '['+ ','.join(array) +']')
